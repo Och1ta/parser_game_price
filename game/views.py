cce35bffeb76d11fa36ea14050eeda11bf7387f5
Anton Kabanov <imcreativchik@gmail.com>
2024-02-14 19:50:30 +0500

    added saving data from the parser to the database

--- game/views.py
+++ game/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 
-from .models import GameName
+from constans import URL_STEAM_SEARCH
+from utils import parser_games_steam
+from .models import Game
 
 
 def get_game_name(request):
     if request.method == 'POST':
         data_input = request.POST.get('data_input')
-        if not GameName.objects.filter(name=data_input).exists():
-            new_data = GameName(name=data_input)
-            new_data.save()
+        if not Game.objects.filter(name=data_input).exists():
+            new_data = parser_games_steam(
+                URL_STEAM_SEARCH.format(data_input),
+                data_input
+            )
+            game_data = Game.objects.create(
+                name=new_data[0],
+                price=new_data[1],
+                url=new_data[2]
+            )
+            game_data.save()
             return redirect('success_page')  # Перенаправляем на страницу успешного сохранения
         else:
             # Данные уже существуют в базе данных, обработка ошибки или вывод сообщения
