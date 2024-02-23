32ab44e6f7574387ffbd16eaaa813f112ee50d10
Anton Kabanov <imcreativchik@gmail.com>
2024-02-24 01:30:38 +0500

    rename url for registration

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/registration/', include('user.urls'), name='registration'),
+    path('auth/registration', include('user.urls'), name='users'),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('game.urls'), name='games'),
 ]
