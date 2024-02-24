794cf68a233f30d497501291b869a25c7ad97963
Anton Kabanov <imcreativchik@gmail.com>
2024-02-24 17:36:41 +0500

    refactor: rename url name for registration

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/registration', include('user.urls'), name='users'),
+    path('auth/registration', include('user.urls'), name='registration'),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('game.urls'), name='games'),
 ]
