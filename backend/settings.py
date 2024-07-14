d60ca88cb7f6b04face64ea159b59a37ffcd0286
Anton Kabanov <imcreativchik@gmail.com>
2024-07-14 15:42:37 +0500

    Added settings for celery

--- backend/settings.py
+++ backend/settings.py
@@ -127,3 +127,26 @@ DJOSER = {
     'HIDE_USERS': False,
     'LOGIN_FIELD': 'email',
 }
+
+CELERY_TIMEZONE = 'Europe/Moscow'
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['json']
+
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_BEAT_SCHEDULE = {
+    'check_price_game_task': {
+        'task': 'backend.tasks.check_price_game_task',
+        'schedule': 30.0,
+    },
+}
