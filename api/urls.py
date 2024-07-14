dfb03ed06d7457865ac1674341c5f3d3831081a7
Anton Kabanov <imcreativchik@gmail.com>
2024-07-14 15:43:04 +0500

    Added urls for djoser auth

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import GameViewSet, CustomUserViewSet, FavoriteGameViewSet
 
+
 app_name = 'api'
 
 router = DefaultRouter()
@@ -13,5 +14,6 @@ router.register('favorite', FavoriteGameViewSet, basename='favorite')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
